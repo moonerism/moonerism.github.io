0b84643c6c87c25725e086b5cf33b90f7c08959f
moonerism <moonerism1@gmail.com>
2017-03-04 00:00:03 +0500

    beta test 1

---
+++ scripts/game.js
@@ -0,0 +1,68 @@
+//Переменные комнат
+//beentopreproom = true;
+//Переменные предметов
+sword = false;
+//Текущая комната
+currentroom = "preproom";
+
+$(document).ready(function() {
+
+  $("#console").fadeIn(2000);
+
+  $("form").submit(function() {
+    var input = $("#command_line").val();
+    var check = false;
+
+    function check() {
+      check = true;
+    }
+
+    // ПОМОЩЬ
+    if (input == "помощь") {
+      $("#message_help").clone().insertBefore("#placeholder").fadeIn(1000);
+      check();
+    }
+    //
+
+    //ВЗЯТЬ ПРЕДМЕТ
+    if (input == "взять меч" && currentroom == "preproom") {
+      $("<p>Ты взял меч.</p>").hide().insertBefore("#placeholder").fadeIn(1000);
+      check();
+    }
+    else if (input == "взять меч" && currentroom != "preproom") {
+      $("<p>Здесь нет меча!</p>").hide().insertBefore("#placeholder").fadeIn(1000);
+      check();
+    }
+    //
+
+    //ПЕРЕДВИЖЕНИЕ
+    if (input == "в бой" && currentroom == "preproom") {
+      currentroom = "arena";
+      $("<p>Ты вошел на Арену.</p>").hide().insertBefore("#placeholder").fadeIn(1000);
+      check();
+    }
+    else if (input == "в бой" && currentroom != "preproom") {
+      $("<p>Ты уже на Арене.</p>").hide().insertBefore("#placeholder").fadeIn(1000);
+      check();
+    }
+
+    if (input == "в зал ожидания" && currentroom == "arena") {
+      currentroom = "preproom";
+      $("<p>Ты вернулся в Зал Ожидания.</p>").hide().insertBefore("#placeholder").fadeIn(1000);
+      check();
+    }
+
+    //
+
+    //ОШИБОЧНАЯ КОМАНДА
+    else if(check == false) {
+      $("<p>Твоя просьба " + input + " непонятна.</p>").hide().insertBefore("#placeholder").fadeIn(1000);
+    }
+    //
+
+    //СБРОС ТЕКСТА
+    $("#command_line").val("");
+    //
+
+  });
+});
